ee1c1b94a257bec5d693f0df1caa1b35b9c4b0cd
alprak <you@example.com>
2018-03-05 20:23:06 +0700

    editted

--- exercise-11.js
+++ exercise-11.js
@@ -5,7 +5,7 @@ function balikKata(kata) {
   // you can only write your code here!
   var kataReverse ="";
   var jumlahKata=kata.length;
-  for(var i=kata.length ;i>=0;i--) {
+  for(var i=kata.length-1 ;i>=0;i--) {
     kataReverse = kataReverse+kata[i];
   }
   return(kataReverse);
